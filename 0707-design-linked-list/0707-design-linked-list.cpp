092314ad40ad6079effd2e943638eaa976cb3f44
Shivam Chaudhary <shivam.mail111311@gmail.com>
2026-09-09 23:52:49 +0530

    Time: 4 ms (81.04%), Space: 25.7 MB (21.93%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.cpp
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -1,14 +1,18 @@
 class MyLinkedList {
 public:
-    struct Node{
+    struct node{
         int val;
-        Node*next;
-        Node(int val){
-            this->val=val;
-            this->next=nullptr;
+        node* next;
+        node(int val1,node* next1){
+            val=val1;
+            next=next1;
+        }
+        node(int val1){
+            val=val1;
+            next=nullptr;
         }
     };
-    Node*head;
+    node* head;
     int size;
     MyLinkedList() {
         head=nullptr;
@@ -16,73 +20,85 @@ public:
     }
     
     int get(int index) {
-        if (index<0 || index>=size) return -1;
-        Node*curr=head;
-        for (int i=0;i<index;i++){
-            curr=curr->next;
+        if(index<0 || index>=size){
+            return -1;
         }
-        return curr->val;
+        node* temp=head;
+        while(index!=0){
+            temp=temp->next;
+            index--;
+        }
+        return temp->val;
     }
     
     void addAtHead(int val) {
-        Node*newNode= new Node(val);
-        newNode->next=head;
-        head=newNode;
+        
+        node* newhead=new node(val);
+        newhead->next=head;
+        head=newhead;
         size++;
+        // return head;
     }
     
     void addAtTail(int val) {
-        Node*newNode=new Node(val);
-        if (head==nullptr){
-            head=newNode;
-            // size++;
+        node* temp=head;
+        if(size==0){
+            head=new node(val);
+            size++;
+            return ;
         }
-        else{
-            Node* curr = head;
-            while (curr->next != nullptr){
-                curr=curr->next;
-            }
-            curr->next=newNode;
+        while(temp->next != nullptr){
+            temp=temp->next;
         }
+        temp->next = new node(val);
         size++;
+        // return temp;
     }
     
     void addAtIndex(int index, int val) {
-        if (index > size) return ;
-        if (index <= 0){
+        if(index < 0 || index > size) { return; } 
+        if(index == 0) { 
             addAtHead(val);
-            return ;
+             return; 
+        } 
+        if(index == size) {
+             addAtTail(val); 
+             return; 
         }
-        if (index==size){
-            addAtTail(val);
-            return ;
+        node* temp=head;
+        int i=0;
+        while(i != index-1){
+            temp=temp->next;
+            i++;
         }
-        Node*newNode= new Node(val);
-        Node*curr=head;
-        for (int i=0;i<index-1;i++){
-            curr=curr->next;
-        }
-        newNode->next=curr->next;
-        curr->next=newNode;
+        node* curr=new node(val);
+        node* front=temp->next;
+        temp->next=curr;
+        curr->next=front;
         size++;
+
     }
     
     void deleteAtIndex(int index) {
-        if (index<0 || index>=size) return ;
-        if (index==0){
-            Node* temp=head;
-            head=head->next;
-            delete temp;
-            size--;
-            return ;
+        node* temp=head;
+        if(index<0 || index>=size){
+            return;
         }
-        Node*curr=head;
-        for (int i=0;i<index-1;i++){
-            curr=curr->next;
+        if(index == 0) {
+            node* deletenode = head;
+            head = head->next; 
+            delete deletenode; 
+            size--; 
+            return;
         }
-        Node*temp=curr->next;
-        curr->next=curr->next->next;
-        delete temp;
+        int i=0;
+        while(i != index-1){
+            temp=temp->next;
+            i++;
+        }
+        node* deletenode=temp->next;
+        temp->next=temp->next->next;
+        delete(deletenode);
         size--;
     }
 };
