72272035c9edfcba3986ca6ee1bcc1b7669abcc3
Shivam Chaudhary <shivam.mail111311@gmail.com>
2026-09-18 01:43:38 +0530

    Time: 98 ms (12.2%), Space: 113.3 MB (20.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for (int i=0;i<nums.size()-1;i++){
-            if (nums[i]==nums[i+1]){
+        unordered_map<int,int>mp;
+        for(auto it: nums){
+            mp[it]++;
+        }
+        for(auto it: mp){
+            if(it.second>=2){
                 return true;
             }
-        
+            
         }
         return false;
-
     }
 };
